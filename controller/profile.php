3c96869d3884488822bf10af8a12e85a424ef102
ab-dereje <abudereje09@gmail.com>
2023-06-26 17:23:09 -0400

    profile is done

--- controller/profile.php
+++ controller/profile.php
@@ -3,15 +3,16 @@
 
     $conn =require_once('../model/database.php');
 session_start();
-$email = $_SESSION["user_email"]["email"];
+$name = $_SESSION["user"]["name"];
+$email = $_SESSION["user"]["email"];
 $sql2 = "SELECT name FROM User WHERE email = '$email'";
-$result1= mysqli_query($conn, $sql2);
-$user = mysqli_fetch_assoc($result1);
-$user_name=$user["name"];
+// $result1= mysqli_query($conn, $sql2);
+// $user = mysqli_fetch_assoc($result1);
+// $user_name=$user["name"];
 
 $div1="";
 $div1 .= "<div class='full-name'>
-<h3>$user_name</h3>
+<h3>$name</h3>
 <h5>$email</h5>
 
 </div>";
